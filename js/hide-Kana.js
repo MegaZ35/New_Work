9725d8024b7b82b5f3c84a36e5af86241c72f2e4
MegaZ35 <heroesz905@gmail.com>
2024-10-25 16:57:14 +0100

    add first commit

---
+++ js/hide-Kana.js
@@ -0,0 +1,12 @@
+function lisTenToUser(){
+    lef secondKana = document.getElementById("second-Kana");
+    secondkana.addEventListener("click", HidesecondKana;)
+}
+
+
+function HidesecondKana(){
+    let secondKana =document.getElementById("second-Kana");
+    secondKana.style.visibility = "hidden"
+}
+
+lisTenToUser();
